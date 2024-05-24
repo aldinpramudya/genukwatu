37398b986f1560da7cd12b6f013138a8fbff969a
aldinpramudya <aldinarielpramudya@gmail.com>
2024-05-24 14:33:00 +0700

    add : fitur CRUD Surat masuk - keluar

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,7 @@ use App\Http\Controllers\AuthController;
 Route::get('/', [App\Http\Controllers\ProfilDesaController::class, 'index'])->name('profil');
 Route::get('/sejarah-desa', [App\Http\Controllers\SejarahDesaController::class, 'index'])->name('sejarah');
 Route::get('/data-kependudukan', [App\Http\Controllers\DataKependudukanController::class, 'index'])->name('data-kependudukan');
-Route::get('/surat-masuk', [App\Http\Controllers\SuratMasukController::class, 'index'])->name('surat-masuk');
-Route::get('/surat-keluar', [App\Http\Controllers\SuratKeluarController::class, 'index'])->name('surat-keluar');
 Route::get('/kontak', [App\Http\Controllers\KontakController::class, 'index'])->name('kontak');
-
 
 Route::get('/login', function () {
     return view('login'); // Mengarahkan ke halaman login
@@ -21,3 +18,19 @@ Route::get('/data-kependudukanadmin', function () {
     // Contoh halaman dashboard
     return view('data-kependudukan');
 })->name('data-kependudukanadmin');
+
+// Route Fitur Surat Masuk
+Route::get('/surat-masuk', [App\Http\Controllers\SuratMasukController::class, 'index'])->name('surat-masuk');
+Route::post('/add-surat-masuk', [App\Http\Controllers\SuratMasukController::class, 'store'])->name('surat-masuk.submit');
+Route::put('/edit-surat-masuk/{id}', [App\Http\Controllers\SuratMasukController::class, 'update'])->name('surat-masuk.edit');
+Route::delete('/surat-masuk/{id}', [App\Http\Controllers\SuratMasukController::class, 'destroy'])->name('surat-masuk.destroy');
+
+// Route Fitur Surat Keluar
+Route::get('/surat-keluar', [App\Http\Controllers\SuratKeluarController::class, 'index'])->name('surat-keluar');
+Route::post('/add-surat-keluar', [App\Http\Controllers\SuratKeluarController::class, 'store'])->name('surat-keluar.submit');
+Route::put('/edit-surat-keluar/{id}', [App\Http\Controllers\SuratKeluarController::class, 'update'])->name('surat-keluar.edit');
+Route::delete('/surat-keluar/{id}', [App\Http\Controllers\SuratKeluarController::class, 'destroy'])->name('surat-keluar.destroy');
+
+
+
+
